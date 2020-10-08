0253a76578025b6d2b45b2c629e72c06e078bd76
Md. Rafiqul Islam <ribijoy19@gmail.com>
2020-10-08 10:53:01 +0600

    portfolio complete

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from portfolio import settings
 import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
